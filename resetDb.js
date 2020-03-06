a0c89b385e72841aeae6883063c28da4cc0363ac
Christopher Duggan <christopherduggan@gmail.com>
2020-03-05 20:02:40 -0500

    create resetDb.js

---
+++ resetDb.js
@@ -0,0 +1,14 @@
+const { db } = require('./models')
+
+async function resetDb() {
+  try {
+    await db.sync({force: true})
+    console.log('database sync\'d')
+  } catch (e) {
+    console.log(e)
+  } finally {
+    process.exit()
+  }
+}
+
+resetDb()
