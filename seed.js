e6c13c493e59041c60d69753bd67c89a17baad13
Christopher Duggan <christopherduggan@gmail.com>
2020-03-05 19:56:32 -0500

    create seed data

---
+++ seed.js
@@ -0,0 +1,41 @@
+const { User, Stock } = require('./models');
+
+// delete database contents
+
+const main = async () => {
+  await User.destroy({
+    where: {}
+  });
+
+  await Stock.destroy({
+    where: {}
+  });
+
+// default user
+  await User.create({
+    username: 'Ruberto Duck',
+    email: 'wet@bird.com',
+    password: 'quack',
+    balance: 5000
+  });
+
+  await Stock.create({
+    companyName: 'Apple, Inc.',
+    ticker: 'BDT',
+    qty: 100,
+    userId: 1
+  })
+
+}
+
+const run = async () => {
+  try {
+    await main();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await process.exit();
+  }
+}
+
+run();
