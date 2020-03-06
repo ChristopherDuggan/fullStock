da7dc1db64a1dc191c649da9fb4f11323a2c2e49
Christopher Duggan <christopherduggan@gmail.com>
2020-03-06 18:32:59 -0500

    prepping for heroku deployment

--- models.js
+++ models.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize({
-  database: 'fullstock_db',
+const db = new Sequelize(
+  (process.env.DATABASE_URL || 'postgres://localhost:5432/fullstock-cd'),
+  {
+  database: 'fullstock-cd',
   dialect: 'postgres',
   define: {
     underscored: true,
