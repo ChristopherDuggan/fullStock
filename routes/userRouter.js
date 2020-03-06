f4b7a72a24ad1bfc4cd6af292b4ad8e663539ba3
Christopher Duggan <christopherduggan@gmail.com>
2020-03-05 21:40:41 -0500

    create userRouter.js

---
+++ routes/userRouter.js
@@ -0,0 +1,70 @@
+const { Router } = require('express')
+const userRouter = Router();
+const { User, Stock } = require('../models');
+const bodyParser = require('body-parser');
+
+userRouter.use(bodyParser.json());
+
+//sign in
+userRouter.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findAll({
+      where: {
+        email: email,
+        password: password
+      }
+    })
+
+    res.json(user[0])
+
+  } catch (e) {
+    console.log('error signing in')
+    res.status(400).json({ msg: e.status });
+  }
+});
+
+//register a new user
+userRouter.post('/register', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.send(user)
+  } catch (e) {
+    res.status(400).json({ msg: e.status });
+  }
+})
+
+//alter a user
+userRouter.put('/:id/update', async (req, res) => {
+  try {
+    const id = req.params.id
+    const editUser = await User.findByPk(id)
+    if (editUser) await editUser.update(req.body)
+    res.send(editUser)
+  } catch (e) {
+    res.status(400).json({ msg: e.status });
+  }
+})
+
+//add a stock
+userRouter.post('/addstock', async (req, res) => {
+  try {
+    const stock = await Stock.create(req.body)
+    res.send(stock)
+  } catch (e) {
+    res.status(400).json({ msg: e.status });
+  }
+})
+
+//get all of a user's stocks
+userRouter.get('/:id/getallstocks', async (req, res) => {
+  try {
+    const id = req.params.id
+    const stocks = await Stock.findAll({ where: { user_id: id } })
+    res.send(stocks)
+  } catch (e) {
+    res.status(400).json({ msg: e.status });
+  }
+})
+
+module.exports = { userRouter }
