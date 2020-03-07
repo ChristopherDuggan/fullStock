221453eef4a8de8f0798df73b1596a906b8e54bb
Christopher Duggan <christopherduggan@gmail.com>
2020-03-06 19:44:20 -0500

    still trying

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const URL = 'https://fullstock-cd.herokuapp.com/4665'
+const URL = process.env.REACT_APP_BASE_URL
 const STOCKSAPI = 'https://cloud.iexapis.com/stable'
 const apikey = process.env.REACT_APP_APIKEY
 
