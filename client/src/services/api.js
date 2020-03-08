34674717a90ff3e9e97f87924a33fc536cb97570
Christopher Duggan <christopherduggan@gmail.com>
2020-03-07 23:19:07 -0500

    heroku redeploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-const URL = 'http://localhost:3001'
+const URL = process.env.REACT_APP_BASE_URL
 const STOCKSAPI = 'https://cloud.iexapis.com/stable'
-const apikey = 'pk_0942ab3ca4d34f93b4e7e34029501ebf'
+const apikey = process.env.REACT_APP_APIKEY
 
 const api = axios.create({
   baseURL: `${URL}`
@@ -44,7 +44,6 @@ export const createUser = async (data) => {
 // update the balance of the user
 export const updateBalance = async (id, data) => {
   try {
-debugger
     const resp = await api.put(`/user/${id}/update`, data)
     return resp
   } catch (e) {
